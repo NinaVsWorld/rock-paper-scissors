e200394ae61b189691d51e12260ec00894760cd5
NinaVsWorld <130551356+NinaVsWorld@users.noreply.github.com>
2026-01-17 07:26:04 +1100

    Complete logic for human and computer choice of weapond

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Link start button to game page
+/*const startButton = document.querySelector(".start-button");
+startButton.addEventListener("click", () => {
+    window.location.replace("index.html");
+});*/
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,27 +13,62 @@ function getRandomInt(max) {
 }
 
 // Randomly generates computer choice between rock, paper or scissors
+// Returns the choice as a string
 function getComputerChoice() {
     const max = 3; 
     const choices = ["rock", "paper", "scissors"];
-    return choices[getRandomInt(max)];
+    const choice = choices[getRandomInt(max)];
+    const computerChoice = document.querySelector(".computer-choice");
+    const img = document.createElement("img");
+
+    if (choice === "rock") {
+        computerChoice.removeChild(computerChoice.lastElementChild);
+        img.src = "icons/draw.png";
+        computerChoice.appendChild(img);
+    } else if (choice === "paper") {
+        computerChoice.removeChild(computerChoice.lastElementChild);
+        img.src = "icons/drawing.png";
+        computerChoice.appendChild(img);
+    } else if (choice === "scissors") {
+        computerChoice.removeChild(computerChoice.lastElementChild);
+        img.src = "icons/scissor-tool.png";
+        computerChoice.appendChild(img);
+    }
+    return choice;
 }
+
+getComputerChoice();
 
 // Prompt user for choice out of rock, paper or scissors and return
 // their choice as a string
-function getHumanChoice() {
-    let choice = prompt("Choose rock, paper or scissors");
-    switch (choice.toLowerCase()) {
-        case "rock":
+// Add event listener to each button, rock paper or scissors
+// The function should return a string which we can use to determine winners in playRound
+const getHumanChoice = function() {
+    const parentContainer = document.querySelector(".container");
+    const humanChoice = document.querySelector(".human-choice");
+    const img = document.createElement("img");
+
+    parentContainer.addEventListener("click", (event) => {
+        if (event.target && (event.target.matches(".rock") || event.target.matches(".rock-img"))) {
+            humanChoice.removeChild(humanChoice.lastElementChild);
+            img.src = "icons/draw.png";
+            humanChoice.appendChild(img);
             return "rock";
-        case "paper":
+        } else if (event.target && (event.target.matches(".paper") || event.target.matches(".paper-img"))) {
+            humanChoice.removeChild(humanChoice.lastElementChild);
+            img.src = "icons/drawing.png";
+            humanChoice.appendChild(img);
             return "paper";
-        case "scissors":
+        } else if (event.target && (event.target.matches(".scissors") || event.target.matches(".scissors-png"))) {
+            humanChoice.removeChild(humanChoice.lastElementChild);
+            img.src = "icons/scissor-tool.png";
+            humanChoice.appendChild(img);
             return "scissors";
-        default:
-            return getHumanChoice();
-    }
+        }
+    });
 }
+
+getHumanChoice();
 
 function playRound(humanChoice, computerChoice) {
    if (humanChoice === computerChoice) {
